a2d46381ff0597d72016492d19c5ceb1be34098a
Brad <brthomiu@gmail.com>
2022-11-15 09:34:14 -0700

    redeploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import { React } from "react";
 import CalculatorBody from "./components/CalculatorBody";
 
 const App = () => {
   return (
+    <>
     <div className="App">
       <CalculatorBody />
     </div>
+    </>
   );
 };
 
